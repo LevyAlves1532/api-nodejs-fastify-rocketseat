24a4720a94ae2b94777be8af85e1312c80444573
Lêvy Alves <levy.pereiraA1532@gmail.com>
2026-04-08 18:27:10 -0300

    Listagem das transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,6 +5,24 @@ import type { FastifyInstance } from 'fastify'
 import { knex } from '../database.js'
 
 export async function transcationsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamSchema = z.object({
+      id: z.uuid(),
+    })
+
+    const { id } = getTransactionParamSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     // { title, amount, type: credit ou debit }
 
