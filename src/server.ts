8ba058bc96feb77cf553c62ae6e1fef65bcdea47
Lêvy Alves <levy.pereiraA1532@gmail.com>
2026-04-18 09:55:04 -0300

    Testando criação de transação

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,33 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
-// import crypto from 'node:crypto'
+// import fastify from 'fastify'
+// import cookie from '@fastify/cookie'
+// // import crypto from 'node:crypto'
 
-import { env } from './env/index.js'
-import { transcationsRoutes } from './routes/transactions.js'
+// import { env } from './env/index.js'
+// import { transcationsRoutes } from './routes/transactions.js'
 
-const app = fastify()
+// const app = fastify()
 
-// GET, POST, PUT, PATCH, DELETE
+// // GET, POST, PUT, PATCH, DELETE
 
-// http://localhost:3333/hello
+// // http://localhost:3333/hello
 
-app.register(cookie)
+// app.register(cookie)
 
-// Contexto global da aplicação
-// app.addHook('preHandler', async (request, reply) => {
-//   console.log(`[${request.method}] ${request.url}`)
+// // Contexto global da aplicação
+// // app.addHook('preHandler', async (request, reply) => {
+// //   console.log(`[${request.method}] ${request.url}`)
+// // })
+
+// app.register(transcationsRoutes, {
+//   prefix: 'transactions',
 // })
 
-app.register(transcationsRoutes, {
-  prefix: 'transactions',
-})
+// app.get('/hello', () => {
+//   return 'Hello World'
+// })
 
-app.get('/hello', () => {
-  return 'Hello World'
-})
+import { env } from './env/index.js'
+import { app } from './app.js'
 
 app
   .listen({
--- test/exemple.spec.ts
+++ test/exemple.spec.ts
@@ -1,8 +1,22 @@
-import { expect, test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app.js'
 
-test('o usuário consegue criar uma nova transação', () => {
-  // Fazer a chamada HTTP p/ criar uma nova transação
-  const responseStatusCode = 201
-  // Validação
-  expect(responseStatusCode).toEqual(201)
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('User can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
